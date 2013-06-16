c0241369562a8eac2eafbbcf431cdb42daf178a3
Alexandru Rada <alexandru.rada@yahoo.com>
2013-06-16 15:47:45 +0300

    -removed unnecessary curl options

--- php/VibetraceClass.php
+++ php/VibetraceClass.php
@@ -30,8 +30,6 @@ class EventTracker {
         //curl
         $ch = curl_init();
         curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
